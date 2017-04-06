0ca4d7a85d78b02f67855e149ba0abe7994810d7
isabellarner <larner.isabel@gmail.com>
2017-04-06 14:48:23 +0100

    Adds fourth test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -12,4 +12,8 @@ describe 'fizzbuzz' do
  it 'returns fizzbuzz if passed 15' do
    expect(fizzbuzz(15)).to eq 'fizzbuzz'
  end
+
+ it 'return fizz if passed 6' do
+   expect(fizzbuzz(6)).to eq 'fizz'
+ end
 end
