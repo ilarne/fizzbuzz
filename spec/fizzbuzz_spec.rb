3568271ed00e1d823f6e9441b8be2bea0fb6d96c
isabellarner <larner.isabel@gmail.com>
2017-04-06 14:46:15 +0100

    Adds third test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -8,4 +8,8 @@ describe 'fizzbuzz' do
  it 'returns buzz if passed 5' do
    expect(fizzbuzz(5)).to eq 'buzz'
  end
+
+ it 'returns fizzbuzz if passed 15' do
+   expect(fizzbuzz(15)).to eq 'fizzbuzz'
+ end
 end
